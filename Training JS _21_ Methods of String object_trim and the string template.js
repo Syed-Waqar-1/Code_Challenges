62693fd4b89122d0f3dcfe75c0439b9ba20f6e24
Syed Waqar <syedwaqarkhalid99@gmail.com>
2022-06-24 18:43:23 +0500

    added on 24-06-22

---
+++ Training JS _21_ Methods of String object_trim and the string template.js
@@ -0,0 +1,26 @@
+// Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+// Note2: Using a string template can make your job easier.
+
+// Example:
+
+// fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+// a
+// aa
+// aaa
+// aaaa
+// aaaaa       <---The effect when you console.log it
+
+function fiveLine(s){
+    let a = s.trim()
+    str = '';
+ for(let i = 1 ; i <= 5; i++){
+     for(let j = 0; j < i; j++){
+       str += a
+     }
+     str +='\n'
+   }return str
+   
+ }
+ fiveLine("  a")
